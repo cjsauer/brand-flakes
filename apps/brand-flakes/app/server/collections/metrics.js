b8702d5f45349f755607325f17d4243b8517f7b5
Calvin Sauer <calvin.j.sauer@gmail.com>
2015-11-12 17:36:46 -0500

    Wrap an Astro class around the Metrics collection

--- apps/brand-flakes/app/server/collections/metrics.js
+++ apps/brand-flakes/app/server/collections/metrics.js
@@ -1,5 +1,33 @@
 Metrics = new Mongo.Collection('metrics');
 
+Metric = Astro.Class({
+  name: 'Metric',
+  collection: Metrics,
+  fields: {
+    name: 'null',
+    appId: {
+      type: 'string',
+      immutable: true
+    },
+    users: {
+      type: 'object',
+      default: function() {
+        return {};
+      }
+    }
+  },
+  simpleValidators: {
+    name: 'required,string,maxLength(30)'
+  },
+  behaviors: ['timestamp'],
+  events: {
+    'beforeInsert': function(e) {
+      // Set the app id to a random 30 character string
+      this.set('appId', Random.id(30));
+    }
+  }
+});
+
 Metrics.allow({
   insert() { return false; },
   update() { return false; },
--- apps/brand-flakes/app/tests/gagarin/testMetricsCollection.js
+++ apps/brand-flakes/app/tests/gagarin/testMetricsCollection.js
@@ -9,4 +9,58 @@ describe('Metrics collection', function() {
       expect(err.message).to.contain('not defined');
     });
   });
+
+  describe('events', function() {
+    it('should generate a random app id before insertion', function(){
+      return server.execute(function() {
+        var metric = new Metric({name: 'Example App'});
+        metric.save();
+        expect(metric.get('appId')).not.to.be.undefined;
+        expect(metric.get('appId').length).to.equal(30);
+      });
+    });
+  });
+
+  describe('validation', function() {
+
+    it('should return true for valid input', function(){
+      return server.execute(function() {
+        var metric = new Metric({
+          name: 'Example App',
+          users: {
+            'some-user-id': {
+              'some-metric': 10
+            }
+          }
+        });
+        expect(metric.validate()).to.be.true;
+      });
+    });
+
+    it('should return false if no name is provided', function(){
+      return server.execute(function() {
+        var metric = new Metric({});
+        expect(metric.validate()).to.be.false;
+      });
+    });
+
+    it('should return false if the name is too long', function(){
+      return server.execute(function() {
+        var metric = new Metric({
+          name: '31characterssssssssssssssssssss'
+        });
+        expect(metric.validate()).to.be.false;
+      });
+    });
+
+    it('should return false if the name is not a string', function(){
+      return server.execute(function() {
+        var metric = new Metric({
+          name: {oops: 'not a string'}
+        });
+        expect(metric.validate()).to.be.false;
+      });
+    });
+
+  });
 });
