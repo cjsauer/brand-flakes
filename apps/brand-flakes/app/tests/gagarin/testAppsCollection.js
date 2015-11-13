e2315c63c2693b49bc5ced265ed4fbd9eacf3b92
Calvin Sauer <calvin.j.sauer@gmail.com>
2015-11-13 12:02:02 -0500

    Improve App collection security tests

--- apps/brand-flakes/app/tests/gagarin/testAppsCollection.js
+++ apps/brand-flakes/app/tests/gagarin/testAppsCollection.js
@@ -2,9 +2,49 @@ describe('Apps collection', function() {
   var server = meteor();
   var client = browser(server);
 
-  it('should not be writable from the client', function(){
+  before(function() {
+    return server.execute(function() {
+      // Clear all apps
+      Apps.remove({});
+    }).then(function() {
+      return client.promise(function(resolve) {
+        // Create a dummy app
+        Meteor.call('apps/insert', new App({name: 'Dummy}'}), function(err, res) {
+          // Save its ID
+          Meteor.dummyId = res;
+          resolve(res);
+        });
+      });
+    });
+  });
+
+  it('should not be insertable from the client', function(){
     return client.promise(function(resolve, reject) {
       Apps.insert({name: 'not allowed!'}, function(err, res) {
+        if(err) {
+          reject(err);
+        }
+      });
+    }).expectError(function (err) {
+      expect(err.message).to.contain('Access denied');
+    });
+  });
+
+  it('should not be removable from the client', function(){
+    return client.promise(function(resolve, reject) {
+      Apps.remove({_id: Meteor.dummyId}, function(err, res) {
+        if(err) {
+          reject(err);
+        }
+      });
+    }).expectError(function (err) {
+      expect(err.message).to.contain('Access denied');
+    });
+  });
+
+  it('should not be editable from the client', function(){
+    return client.promise(function(resolve, reject) {
+      Apps.update({_id: Meteor.dummyId}, {$set: {name: 'HAHAHA'}}, function(err, res) {
         if(err) {
           reject(err);
         }
