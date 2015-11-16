9ccd73e442ea30fe9ee183f20dccd17d5ec4ec40
Calvin Sauer <calvin.j.sauer@gmail.com>
2015-11-16 18:04:52 -0500

    Simplify the insert method tests

--- apps/brand-flakes/app/tests/gagarin/testAppsInsertMethod.js
+++ apps/brand-flakes/app/tests/gagarin/testAppsInsertMethod.js
@@ -1,31 +1,24 @@
 describe('apps/insert method', function() {
   var server = meteor();
-  var client = browser(server);
 
   it('should check its arguments', function(){
-    return client.promise(function(resolve, reject) {
-      Meteor.call('apps/insert', 'a string is not valid!', function(err, res) {
-        if(err) {
-          reject(err);
-        }
-      });
-    }).expectError(function(err) {
-      expect(err).to.be.an.instanceof(Error);
-      expect(err.message).to.contain('Match failed');
+    return server.execute(function() {
+      var badArgsCall = function() {
+        Meteor.call('apps/insert', 'a string is not valid!');
+      }
+      
+      expect(badArgsCall).to.throw(Error, /Match error/);
     });
   });
 
   it('should throw if the App is not valid', function(){
-    return client.promise(function(resolve, reject) {
-      var app = new App({/* no fields set */});
-      Meteor.call('apps/insert', app, function(err, res) {
-        if(err) {
-          reject(err);
-        }
-      });
-    }).expectError(function(err) {
-      expect(err).to.be.an.instanceof(Error);
-      expect(err.message).to.contain('validation-error');
+    return server.execute(function() {
+      var invalidAppCall = function() {
+        var app = new App({/* no fields set */});
+        Meteor.call('apps/insert', app);
+      }
+      
+      expect(invalidAppCall).to.throw(Error, /validation-error/);
     });
   });
 
