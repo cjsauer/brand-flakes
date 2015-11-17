7fcba18c6c07e7e0dd134c8e958a1ea380a541fe
Calvin Sauer <calvin.j.sauer@gmail.com>
2015-11-17 15:15:20 -0500

    Clean up connection on failure

--- src/brand-flakes.js
+++ src/brand-flakes.js
@@ -1,4 +1,4 @@
-(function(Meteor) {
+(function() {
   var host;
   if(Meteor && Meteor.settings.public.brandFlakes) {
     // For local development, we can set the server in the settings.json file
@@ -7,7 +7,13 @@
     host = "NO HOST SET";
   }
 
+  // Don't try to reconnect automatically. If the given
+  // appId is not valid, we want to be able to close
+  // the connection.
   var server = new Asteroid(host);
+  server.ddp._autoreconnect = false;
+
+  // Fetch [appId, userId]
   var qCommands = BrandFlakes.q.pop();
 
   // Authenticate the app
@@ -17,6 +23,8 @@
         console.log("APP AUTHENTICATED!");
       }
     }).catch(function(err) {
+      // Clean up
+      server.ddp._socket.close();
       console.error(err);
     });
-})(Meteor);
+})();
