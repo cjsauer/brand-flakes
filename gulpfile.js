6e247fc04c752fced3118119efc551fd7de77062
Calvin Sauer <calvin.j.sauer@gmail.com>
2015-11-17 14:45:52 -0500

    Fix order of gulp concats

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 
 gulp.task('build', function() {
-  gulp.src(['src/*.js', './bower_components/ddp.js/src/ddp.js', './bower_components/q/q.js', './bower_components/asteroid/dist/asteroid.browser.js'])
+  gulp.src(['./bower_components/ddp.js/src/ddp.js', './bower_components/q/q.js', './bower_components/asteroid/dist/asteroid.browser.js', 'src/*.js'])
     .pipe(concat('brand-flakes.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('./apps/brand-flakes/app/public'));
